006eb4cb193d3ee0b582e57a4a3eecfc11ccc123
Gaurav Soni <e.gauravsoni@gmail.com>
2019-03-15 12:33:25 +0530

    created a web api project to get a list of people

---
+++ Request-Response-API/Request-Response-API/Controllers/MyRequestController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Request_Response_API
+{
+    [Route("api/[controller]")]
+    public class MyRequestController : ControllerBase
+    {
+        // GET api/myrequest
+        [HttpGet]
+        public string Get()
+        {
+            var list = new List<MyRequestModel>();
+
+            list.Add(new MyRequestModel { Name = "Peter", Company = "XYZ", Contact = "********", Email = "Peter@xyz.com", Location = "USA", Skill = "C#" });
+            list.Add(new MyRequestModel { Name = "Jhon", Company = "ABC", Contact = "********", Email = "Jhon@xyz.com", Location = "UK", Skill = "C#" });
+            list.Add(new MyRequestModel { Name = "Oz", Company = "EGF", Contact = "********", Email = "Oz@xyz.com", Location = "Brazil", Skill = "C#" });
+
+            var json = JsonConvert.SerializeObject(list);
+
+            return json;
+        }
+
+        // GET api/myrequest/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/myrequest
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/myrequest/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/myrequest/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Request-Response-API/Request-Response-API/Models/MyRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Request_Response_API
+{
+    public class MyRequestModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public string Skill { get; set; }
+    }
+}
